bcd840df2d3a1fd1b6170ded6ef6327a1fd0bfda
ArunAnappindi <arunanappindi@gmail.com>
2023-10-29 23:05:53 -0500

    Find Max Sum Subarray using Kadane's algorithm

--- src/main/java/com/arrays/FindMaxSumSubArray.java
+++ src/main/java/com/arrays/FindMaxSumSubArray.java
@@ -1,4 +1,41 @@
 package com.arrays;
 
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Implementing Kadane's algorithm to find out the subarray that returns the max sum.
+ *
+ */
 public class FindMaxSumSubArray {
+
+
+    public static void main(String[] args) {
+
+        int[] numArray = {4,3,-2,6,-12,7,-1,6};
+        findMaxSumSubArray(numArray);
+
+    }
+
+    private static void findMaxSumSubArray(int[] numArray) {
+        int currMax = 0;
+        int maxSoFar = 0;
+
+        List<Integer> subArr = new ArrayList<>();
+        for(int i =0; i < numArray.length; i++) {
+            if(numArray[i] + currMax > numArray[i]) {
+                subArr.add(numArray[i]);
+                currMax = numArray[i] + currMax;
+            } else {
+                subArr.clear();
+                subArr.add(numArray[i]);
+                currMax = numArray[i];
+            }
+
+            if(currMax > maxSoFar) {
+                maxSoFar = currMax;
+            }
+        }
+    }
+
 }

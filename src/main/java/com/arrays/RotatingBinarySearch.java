1f650e4dbdd2e537e320fac6815d8e70656929b9
ArunAnappindi <arunanappindi@gmail.com>
2023-11-25 15:33:01 -0600

    added new implementations.

--- src/main/java/com/arrays/RotatingBinarySearch.java
+++ src/main/java/com/arrays/RotatingBinarySearch.java
@@ -4,9 +4,9 @@ public class RotatingBinarySearch {
 
 
     public static void main(String[] args) {
-
+//13,14,15, 16, 17,18, 19, 11,12
         int[]  binaryTree = {13, 14, 15, 1, 2, 3, 7, 9, 10, 11, 12}; //{5, 6, 7, 9, 10, 11, 1, 2, 3};// {13, 14, 15, 1, 2, 3, 7, 9, 10, 11, 12}
-        int target = 14;
+        int target = 13;
         rotatingBinarySearch(binaryTree, target);
     }
 
@@ -18,19 +18,29 @@ public class RotatingBinarySearch {
 
     private static int helper(int[] binaryTree, int target, int start, int end) {
 
+        if(start < 0 || end < 0 || start > end) {
+            return -1;
+        }
         int mid = start + (end - start)/2;
 
         if(binaryTree[mid] == target) {
             return mid;
-        } else if(binaryTree[mid] > target && binaryTree[start] < target) {
-            end = mid-1;
-        } else if(binaryTree[mid] > target && binaryTree[start] > target) {
-            start = mid+1;
-        } else if(binaryTree[mid] < target && binaryTree[start] < target) {
-            end = mid-1;
-        } else if(binaryTree[mid] < target && binaryTree[start] > target) {
-            start = mid+1;
         }
+
+        if( binaryTree[start] <= binaryTree[mid]){
+            if(binaryTree[start] <= target &&  binaryTree[mid] >= target ) {
+                end = mid-1;
+            } else {
+                start = mid + 1;
+            }
+        } else {
+            if(binaryTree[mid] <= target && target <= binaryTree[end]) {
+                start = mid +1;
+            } else {
+                end = mid -1;
+            }
+        }
+
 
         return helper(binaryTree, target, start, end);
 
